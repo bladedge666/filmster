1b9312e68ddabf3970d4c76966217cf0afd7f1b3
Srijal Joshi <bladedge666@gmail.com>
2016-12-02 22:23:16 +0545

    Ch 16. Display individual movie details.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,6 @@
 
-$(function(){
+$(function () {
+
   let form = $('#movie-search');
   form.submit(function(e){
     e.preventDefault();
@@ -11,9 +12,10 @@ $(function(){
     .done(function(data){
       displayMovies(data);
     });
+
   });
 
-  function displayMovies(data){
+  function displayMovies(data) {
     container = $("#movies");
     let htmlString = "";
     container.empty();
@@ -26,7 +28,7 @@ $(function(){
 
       data["Search"].forEach (function(movie) {
         htmlString += `
-        <img src=${ movie["Poster"] == "N/A" ? "/assets/not-found.png" : movie["Poster"] } />
+        <img src=${ movie["Poster"] == "N/A" ? "/assets/not-found.png" : movie["Poster"] } data-id=${ movie["imdbID"] } />
         <p>${movie["Title"]}</p>
         <p>${movie["Year"]}</p>`;
       });
@@ -34,4 +36,46 @@ $(function(){
 
     container.append(htmlString);
   }
+
+
+  $('#movies').on('click', 'img', function(element){
+    element.preventDefault();
+
+    let id = $(element.target).data('id');
+
+    $.ajax({
+      url: "http://www.omdbapi.com/?",
+      data: {i: id}   
+    })
+    .done(function(data){
+      displayMovie(data);
+    });
+
+  });
+
+  function displayMovie(data) {
+    container = $("#movies");
+    let htmlString = "";
+    container.empty();
+
+    if (data["Response"] == "False") {
+      htmlString = `
+      <div class="alert alert-danger text-center" role="alert">${data["Error"]}</div>
+      `;
+    } else {
+
+      htmlString += `<img src=${ data["Poster"] == "N/A" ? "/assets/not-found.png" : data["Poster"] } alt="${data["Title"]}" />`;
+      let count = 0;
+      for (let k in data) {
+        if (k == "imdbID") continue;
+        console.log(k);
+        if (count++ >= 12) break;
+        htmlString += `<p>${ k + ": " + data[k]}</p>`;
+      }
+
+    }
+
+    container.append(htmlString);
+  }
+
 });
