a0be30c8ffbb3c9a52462b5b8cc4928b8781e559
Srijal Joshi <bladedge666@gmail.com>
2016-12-06 13:57:50 +0545

    Add masonry

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -15,80 +15,103 @@ $(function () {
 
   });
 
-  function displayMovies(data) {
-    container = $("#movies");
-    let htmlString = "";
-    container.empty();
+// ---------------Display all the movies------------------------- //
 
-    if (data["Response"] == "False") {
-      htmlString = `
-      <div class="alert alert-danger text-center" role="alert">${data["Error"]}</div>
-      `;
-    } else {
+function displayMovies(data) {
+  container = $("#movies");
+  let htmlString = "";
+  container.empty();
 
-      data["Search"].forEach (function(movie) {
-        htmlString += `
-        <div class="grid-item--width2">
-          <img class="grid-item-img" src=${ movie["Poster"] == "N/A" ? "/assets/not-found.png" : movie["Poster"] } data-id=${ movie["imdbID"] } />
-          <p class="grid-item-para">
-            ${movie["Title"]} <br> ${movie["Year"]}
-          </p>
-        </div>`;
-      });
-    }
 
-    container.append(htmlString);
+  if (data["Response"] == "False") {
+    htmlString = `
+    <div class="alert alert-danger text-center" role="alert">${data["Error"]}</div>
+    `;
+  } else {
+
+    data["Search"].forEach (function(movie) {
+      htmlString += `
+      <div class="grid-item">
+      <img class="grid-item-img" src=${ movie["Poster"] == "N/A" ? "/assets/not-found.png" : movie["Poster"] } data-id=${ movie["imdbID"] } />
+      <p class="grid-item-para">
+      ${movie["Title"]} <br> ${movie["Year"]}
+      </p>
+      </div>`;
+    });
   }
 
+  container.append(htmlString);
+  masonryInit(container);
+}
 
-  $('#movies').on('click', 'img', function(element){
-    element.preventDefault();
 
-    let id = $(element.target).data('id');
+$('#movies').on('click', 'img', function(element){
+  element.preventDefault();
 
-    $.ajax({
-      url: "http://www.omdbapi.com/?",
-      data: {i: id}   
-    })
-    .done(function(data){
-      displayMovie(data);
-    });
+  let id = $(element.target).data('id');
 
+  $.ajax({
+    url: "http://www.omdbapi.com/?",
+    data: {i: id}   
+  })
+  .done(function(data){
+    displayMovie(data);
   });
 
-  
-  function displayMovie(data){
-    let container = $("#movies")
-    let htmlString = "";
-    container.empty();
+});
 
-    htmlString += `
-    <img src=${data["Poster"] == "N/A" ? "http://www.reelviews.net/resources/img/default_poster.jpg" : data["Poster"]}  />
-    <p>Title :  ${data["Title"]}</p>
-    <p>Year:  ${data["Year"]}</p>
-    <p> ${data["Plot"]}</p>                         
-    <p>Released : ${data["Released"]}</p>
-    <p>Runtime : ${data["Runtime"]}</p>
-    <p>Genre : ${data["Genre"]}</p>
-    <p>Director : ${data["Director"]}</p>
-    <p>Writter : ${data["Writer"]}</p>
-    <p>Actors : ${data["Actors"]}</p>
-    <p>Language : ${data["Language"]}</p>
-    <p>Country : ${data["Country"]}</p>
-    <p>Awards : ${data["Awards"]}</p>
 
-    <form id="rating-form" action="/reviews" method="POST">
-    <input type="hidden" name="authenticity_token" value=${window._token} />
-    <input type="hidden" name="imdbid" value=${data["imdbID"]} />
-    <textarea name= "review[comment]" class="form-control" placeholder="Your movie review"/>
-    <br />
-    <input type="submit" class="btn btn-success pull-right" />
-    </form>`;
-    
-    container.append(htmlString);
-  }
+function displayMovie(data){
+  let container = $("#movies")
+  let htmlString = "";
+  container.empty();
+
+  htmlString += `
+  <img style="height:200px;" src=${data["Poster"] == "N/A" ? "http://www.reelviews.net/resources/img/default_poster.jpg" : data["Poster"]}  />
+  <p>Title :  ${data["Title"]}</p>
+  <p>Year:  ${data["Year"]}</p>
+  <p> ${data["Plot"]}</p>                         
+  <p>Released : ${data["Released"]}</p>
+  <p>Runtime : ${data["Runtime"]}</p>
+  <p>Genre : ${data["Genre"]}</p>
+  <p>Director : ${data["Director"]}</p>
+  <p>Writter : ${data["Writer"]}</p>
+  <p>Actors : ${data["Actors"]}</p>
+  <p>Language : ${data["Language"]}</p>
+  <p>Country : ${data["Country"]}</p>
+  <p>Awards : ${data["Awards"]}</p>
+
+  <form id="rating-form" action="/reviews" method="POST">
+  <input type="hidden" name="authenticity_token" value=${window._token} />
+  <input type="hidden" name="imdbid" value=${data["imdbID"]} />
+  <textarea name= "review[comment]" class="form-control" placeholder="Your movie review"/>
+  <br />
+  <input type="submit" class="btn btn-success pull-right" />
+  </form>`;
+
+  container.append(htmlString);
+}
 
   // Masonry init
-
+  function masonryInit (container) {
+    var $container = $(container);
+    
+    $container.imagesLoaded(function() {
+      $container.masonry({
+        itemSelector : '.grid-item',
+      });
+    });
+  }
 
 });
+
+
+
+
+
+
+
+
+
+
+
