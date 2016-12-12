2ba2a9c524e61e0290f4ea1b7b1dcb5b05f3ca80
Srijal Joshi <bladedge666@gmail.com>
2016-12-12 22:47:11 +0545

    Setting up upvotes

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,9 +3,6 @@ class UpvotesController < ApplicationController
 
   def create
     @review = Review.find(params[:review_id])
-    puts "==========================+++++++++++++++++++=#{@review.id}"
-    puts "==========================+++++++++++++++++++=#{current_user.username}"
-
     # Upvote.create({user_id: current_user.id, review_id: @review.id})
     current_user.upvote(@review)
 
